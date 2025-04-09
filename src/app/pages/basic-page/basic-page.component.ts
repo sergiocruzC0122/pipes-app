776e4a07f42b44ab6ac234fca02fb8f425a515fe
sergiocruzC0122 <thiorfthix@gmail.com>
2025-04-09 10:43:26 -0500

    adding date and number PIPE

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,13 +1,28 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import {Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import {Component, effect, signal } from '@angular/core';
+import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProvider';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
   nameLower = signal ('sergio');
   nameUpper = signal ('SERGIO');
   nameName = signal ('sErgIo CrUZ');
+
+
+  customDate = signal ( new Date());
+
+  tickingDateEffect = effect((onCleanup) =>{
+    const interval = setInterval (() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+
+    onCleanup (() => {
+      clearInterval(interval);
+    })
+  })
 }
