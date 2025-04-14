b6f97732fe6aa03abfaab8f8cbfbf1b03459e67e
Sergio Cruz <sergio.cruz@onoff.com.co>
2025-04-14 09:33:10 -0500

    Adding custom gifs

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
+
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [
+    ToggleCasePipe,
+  ],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent {}
+export default class CustomPageComponent {
+
+  name = signal('Nicolas Cruz');
+  upperCase = signal(true)
+  heroes= signal(heroes);
+
+}
