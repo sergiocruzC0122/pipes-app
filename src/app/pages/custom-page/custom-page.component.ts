ffc09c7e589892845323b788c226a4d2dfc7ba58
Sergio Cruz <sergio.cruz@onoff.com.co>
2025-04-15 08:22:49 -0500

    end curse with pipes

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -6,6 +6,9 @@ import { heroColorPipe } from '../../pipes/heroColor-case.pipe';
 import { heroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 import { TitleCasePipe } from '@angular/common';
 import { heroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { heroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { Hero } from '../../interfaces/hero.interface';
+import { heroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 
 @Component({
@@ -17,6 +20,8 @@ import { heroCreatorPipe } from '../../pipes/hero-creator.pipe';
     heroTextColorPipe,
     TitleCasePipe,
     heroCreatorPipe,
+    heroSortByPipe,
+    heroFilterPipe,
   ],
   templateUrl: './custom-page.component.html',
 })
@@ -25,5 +30,9 @@ export default class CustomPageComponent {
   name = signal('Nicolas Cruz');
   upperCase = signal(true)
   heroes= signal(heroes);
+
+  sortBy = signal<keyof Hero | null> (null);
+
+  searchQuery = signal('');
 
 }
---
+++ src/app/pipes/hero-filter.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Hero } from '../interfaces/hero.interface';
+
+@Pipe({
+    name: 'heroFilter'
+})
+
+export class heroFilterPipe implements PipeTransform {
+    transform(value: Hero[], search: string): Hero[] {
+        if(!search) return value;
+
+        search = search.toLowerCase();
+        
+        return value.filter((hero) => hero.name.toLowerCase().includes(search));
+    }
+}
